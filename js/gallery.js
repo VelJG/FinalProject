215d45854c6150fc3058b32b693f57c75eaed166
VelJG <huynhankhuong0511@gmail.com>
2024-10-16 21:27:26 +0700

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,31 @@
+
+let og;
+function load() {
+    og=document.getElementById('image').innerHTML;
+previewImgs = document.getElementsByClassName('preview');
+for (let i = 0; i < previewImgs.length; i++) {
+   console.log('Added');
+	previewImgs[i].setAttribute('tabindex', '0');
+    previewImgs[i].onfocus = function () {
+        upDate(this);
+    };
+    previewImgs[i].onblur = function () {
+        unDo();
+    };
+}
+}
+function upDate(previewPic) {
+   
+	
+ pic=document.getElementById('image'); 
+ pic.style.backgroundImage=`url(${previewPic.src})`;
+  pic.innerHTML = previewPic.alt;
+	}
+
+function unDo(){
+   
+pic=document.getElementById('image');
+	pic.style.backgroundImage='';
+    pic.innerHTML= og;
+}
+
